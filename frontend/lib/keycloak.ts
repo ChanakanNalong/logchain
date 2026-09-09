30b2be2a60d4825afa33e2c86a9ab60db8a3f445
Chanakan Nalong <chanakan111138526@gmail.com>
2026-09-09 22:38:33 +0700

    chore: remove legacy Next.js frontend (superseded by cylis-dashboard)

--- frontend/lib/keycloak.ts
+++
@@ -1,9 +0,0 @@
-import Keycloak from 'keycloak-js';
-
-const keycloak = new Keycloak({
-  url: process.env.NEXT_PUBLIC_KEYCLOAK_URL!,
-  realm: process.env.NEXT_PUBLIC_KEYCLOAK_REALM!,
-  clientId: process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID!,
-});
-
-export default keycloak;
